39d31dd1d4e90ea17d4dd49135054075115ceaf7
Soo-Hyun Yoo <yoos117@gmail.com>
2013-03-16 19:14:55 -0700

    Add ADC files.

---
+++ firmware/osuar_adc.c
@@ -0,0 +1,43 @@
+#include <osuar_adc.h>
+
+void setup_adc(void)
+{
+	/*
+	 * Initialize ADC driver 1 and set the following as inputs: PA4, PA5, PC0,
+	 * PC1, PC2, PC3
+	 */
+	adcStart(&ADCD1, NULL);
+	palSetPadMode(GPIOA, 4, PAL_MODE_INPUT_ANALOG);
+	palSetPadMode(GPIOA, 5, PAL_MODE_INPUT_ANALOG);
+	palSetPadMode(GPIOC, 0, PAL_MODE_INPUT_ANALOG);
+	palSetPadMode(GPIOC, 1, PAL_MODE_INPUT_ANALOG);
+	palSetPadMode(GPIOC, 2, PAL_MODE_INPUT_ANALOG);
+	palSetPadMode(GPIOC, 3, PAL_MODE_INPUT_ANALOG);
+}
+
+void update_adc(void)
+{
+	chSysLockFromIsr();
+	adcStartConversionI(&ADCD1, &adcgrpcfg, samples, ADC_GRP1_BUF_DEPTH);
+	chSysUnlockFromIsr();
+}
+
+/*
+ * ADC end conversion callback.
+ * The PWM channels are reprogrammed using the latest ADC samples.
+ * The latest samples are transmitted into a single SPI transaction.
+ */
+void adccb(ADCDriver *adcp, adcsample_t *buffer, size_t n) {
+
+  (void) buffer; (void) n;
+  /* Note, only in the ADC_COMPLETE state because the ADC driver fires an
+     intermediate callback when the buffer is half full.*/
+  if (adcp->state == ADC_COMPLETE) {
+    adcsample_t avg_ch1, avg_ch2;
+
+    /* Calculates the average values from the ADC samples.*/
+    avg_ch1 = (samples[0] + samples[2] + samples[4] + samples[6]) / 4;
+    avg_ch2 = (samples[1] + samples[3] + samples[5] + samples[7]) / 4;
+  }
+}
+
---
+++ firmware/osuar_adc.h
@@ -0,0 +1,44 @@
+#ifndef OSUAR_ADC_H
+#define OSUAR_ADC_H
+
+#include <ch.h>
+#include <hal.h>
+
+void setup_adc(void);
+void update_adc(void);
+static void adccb(ADCDriver *adcp, adcsample_t *buffer, size_t n);
+
+/* Total number of channels to be sampled by a single ADC operation.*/
+#define ADC_GRP1_NUM_CHANNELS   2
+
+/* Depth of the conversion buffer, channels are sampled four times each.*/
+#define ADC_GRP1_BUF_DEPTH      4
+
+/*
+ * ADC samples buffer.
+ */
+static adcsample_t samples[ADC_GRP1_NUM_CHANNELS * ADC_GRP1_BUF_DEPTH];
+
+/*
+ * ADC conversion group.
+ * Mode:        Linear buffer, 4 samples of 2 channels, SW triggered.
+ * Channels:    IN11   (48 cycles sample time)
+ *              Sensor (192 cycles sample time)
+ */
+static const ADCConversionGroup adcgrpcfg = {
+  FALSE,
+  ADC_GRP1_NUM_CHANNELS,
+  adccb,
+  NULL,
+  /* HW dependent part.*/
+  0,
+  ADC_CR2_SWSTART,
+  ADC_SMPR1_SMP_AN11(ADC_SAMPLE_56),
+  0,
+  ADC_SQR1_NUM_CH(ADC_GRP1_NUM_CHANNELS),
+  0,
+  ADC_SQR3_SQ2_N(ADC_CHANNEL_IN11)
+};
+
+#endif // OSUAR_ADC_H
+
