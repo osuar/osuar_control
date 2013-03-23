7bdb9ae450b553883c0ec892fbcef9dd67569e35
Soo-Hyun Yoo <yoos117@gmail.com>
2013-03-22 19:23:40 -0700

    Move I2C configuration structure to own file.

--- firmware/osuar_ahrs.h
+++ firmware/osuar_ahrs.h
@@ -3,19 +3,11 @@
 
 #include <ch.h>
 #include <hal.h>
+#include <osuar_i2c.h>
 #include <osuar_config.h>
 
 void setup_ahrs(void);
 void update_ahrs(void);
-
-/*
- * I2C configuration structure.
- */
-static const I2CConfig i2c1cfg = {
-	OPMODE_I2C,
-	400000,
-	FAST_DUTY_CYCLE_2,
-};
 
 #endif // AHRS_H
 
--- firmware/osuar_i2c.h
+++ firmware/osuar_i2c.h
@@ -1,7 +1,16 @@
 #ifndef I2C_H
 #define I2C_H
 
-
+/*
+ * I2C configuration structure.
+ *
+ * See $CHIBIOS/os/hal/platforms/STM32/I2Cv1
+ */
+static const I2CConfig i2c1cfg = {
+	OPMODE_I2C,
+	400000,
+	FAST_DUTY_CYCLE_2
+};
 
 #endif // I2C_H
 
