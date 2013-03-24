b943fc8ebdc0129c0e835c2ea084b5a2596e9000
Soo-Hyun Yoo <yoos117@gmail.com>
2013-03-24 00:12:28 -0700

    Poll the magnetometer.

--- firmware/osuar_ahrs.c
+++ firmware/osuar_ahrs.c
@@ -3,10 +3,17 @@
 void setup_ahrs(void)
 {
 	i2cStart(&I2CD1, &i2c1cfg);
+
+	//init_accel();   // TODO
+	//init_gyro();   // TODO
+	init_mag();
 }
 
 void update_ahrs(void)
 {
+	//poll_accel();   // TODO
+	//poll_gyro();   // TODO
+	poll_mag();
 
 }
 
--- firmware/osuar_ahrs.h
+++ firmware/osuar_ahrs.h
@@ -5,6 +5,7 @@
 #include <hal.h>
 #include <osuar_i2c.h>
 #include <osuar_config.h>
+#include <osuar_mag_lsm303.h>   // TODO: This is the magnetometer on the STM32F3 board, for testing purposes, as we do not yet have our flight control board fully assembled.
 
 void setup_ahrs(void);
 void update_ahrs(void);
