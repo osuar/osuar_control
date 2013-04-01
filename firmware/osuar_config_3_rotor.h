77175e535a036f17b809e6f91585f2e86711fd25
Soo-Hyun Yoo <yoos117@gmail.com>
2013-04-01 00:35:38 -0700

    Add basic definitions for tricopter.

---
+++ firmware/osuar_config_3_rotor.h
@@ -0,0 +1,11 @@
+#ifndef OSUAR_CONFIG_3_ROTOR_H
+#define OSUAR_CONFIG_3_ROTOR_H
+
+#define THROTTLE_CAP 0.7   // Maximum duty cycle allowed for any of the rotors.
+
+#define I_ST 0   // Tail servo array index.
+#define I_RT 0   // Tail rotor array index.
+#define I_RR 1   // Right rotor array index.
+#define I_RL 2   // Left rotor array index.
+
+#endif // OSUAR_CONFIG_3_ROTOR_H
