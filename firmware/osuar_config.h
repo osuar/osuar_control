4b49b37430f44ab71afaf3f47f99caccd2844085
Soo-Hyun Yoo <yoos117@gmail.com>
2013-04-01 00:35:06 -0700

    Include separate config files depending on number of rotors.

--- firmware/osuar_config.h
+++ firmware/osuar_config.h
@@ -1,9 +1,45 @@
+/******************************************************************************
+ * TODO: Add a better description.
+ *
+ * Some notes on prefixes: I'll be trying to organize these definitions with
+ * prefixes. So far:
+ *    I_       - Index
+ *****************************************************************************/
+
 #ifndef CONFIG_H
 #define CONFIG_H
 
 #define NUM_ROTORS 4
 #define ESC_COMM SPI   // Set to either PWM or SPI.
 
+
+/******************************************************************************
+ * 2-rotor system.
+ *
+ * Currently, this applies only to the robat.
+ *****************************************************************************/
+#if (NUM_ROTORS == 2)
+#include <osuar_config_2_rotor.h>
+#endif // NUM_ROTORS == 2
+
+/******************************************************************************
+ * 3-rotor system.
+ *
+ * Currently, this applies only to Soo-Hyun's tricopter.
+ *****************************************************************************/
+#if (NUM_ROTORS == 3)
+#include <osuar_config_3_rotor.h>
+#endif // NUM_ROTORS == 3
+
+/******************************************************************************
+ * 4-rotor system.
+ *
+ * Currently, we have two quadrotors. We may or may not decide to keep separate
+ * config files for the two robots.
+ *****************************************************************************/
+#if (NUM_ROTORS == 4)
+#include <osuar_config_4_rotor.h>
+#endif // NUM_ROTORS == 4
 
 #endif // CONFIG_H
 
