711d5011d514bde6a7e88a52969b05e5e22f0f86
Soo-Hyun Yoo <yoos117@gmail.com>
2013-04-01 23:42:57 -0700

    Fix struct typedef.

--- firmware/osuar_pid.h
+++ firmware/osuar_pid.h
@@ -4,13 +4,13 @@
 /**
  * @brief PID parameters struct.
  */
-typedef struct pid_data_t {
+typedef struct {
     uint8_t id;   // Used by calculate_pid().
     float Kp, Ki, Kd;   // Gains.
     float dt;   // Timestep In seconds.
     float last_val;
     float P, I, D;   // Terms.
-};
+} pid_data_t;
 
 /**
  * @brief Calculate PID output.
@@ -19,7 +19,7 @@ typedef struct pid_data_t {
  * @param des Desired value.
  * @param pid_data PID parameters struct.
  */
-float calculate_pid (float cur, float des, struct pid_data_t &pid_data);
+float calculate_pid (float cur, float des, pid_data_t* pid_data);
 
 #endif // OSUAR_PID_H
 
