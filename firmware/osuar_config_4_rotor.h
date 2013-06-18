08834ad67fca82e1390eec505f57018f8fc93e39
Soo-Hyun Yoo <yoos117@gmail.com>
2013-06-18 02:42:24 -0700

    Add config file for quadrotor.

---
+++ firmware/osuar_config_4_rotor.h
@@ -0,0 +1,14 @@
+#ifndef OSUAR_CONFIG_4_ROTOR_H
+#define OSUAR_CONFIG_4_ROTOR_H
+
+#define THROTTLE_CAP 0.7   // Maximum duty cycle allowed for any of the rotors.
+
+// Rotor indices by angle from positive X. For example, I_R045 refers to the
+// rotor at 45 degrees, i.e., front left.
+#define I_R045 0
+#define I_R135 1
+#define I_R225 2
+#define I_R315 3
+
+#endif // OSUAR_CONFIG_4_ROTOR_H
+
