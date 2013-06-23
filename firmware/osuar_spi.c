f76537fe10271628cf843a17ff6d25a7f80ab8d7
Soo-Hyun Yoo <yoos117@gmail.com>
2013-06-23 06:20:57 -0700

    Add a todo comment..

--- firmware/osuar_spi.c
+++ firmware/osuar_spi.c
@@ -5,6 +5,8 @@ void setup_spi(void)
 	palSetPadMode(GPIOB, 10, PAL_MODE_ALTERNATE(5) | PAL_STM32_OSPEED_HIGHEST);   /* SPI3 SCK */
 	palSetPadMode(GPIOB, 11, PAL_MODE_ALTERNATE(5) | PAL_STM32_OSPEED_HIGHEST);   /* SPI3 MISO */
 	palSetPadMode(GPIOB, 12, PAL_MODE_ALTERNATE(5) | PAL_STM32_OSPEED_HIGHEST);   /* SPI3 MOSI */
+
+	/* TODO: May need a mutex when we begin using more than one SPI slave. */
 }
 
 void comm_spi(SPIDriver *spid, const SPIConfig *spicfg, uint8_t *txbuf, uint8_t *rxbuf, uint16_t bufsize)
