28920e4989bd02713c73e0fb767e76061681167c
Soo-Hyun Yoo <yoos117@gmail.com>
2013-06-24 04:28:04 -0700

    Fancy fading LEDs, less obnoxious blinker, subsystem initializations, etc.

--- firmware/main.c
+++ firmware/main.c
@@ -32,11 +32,12 @@ static msg_t comm_thread(void *arg)
 
 	float mag[3];
 
-	while (TRUE) {
-		time += MS2ST(100);
-		counter++;
+	palSetPadMode(GPIOA, 6, PAL_MODE_OUTPUT_PUSHPULL);
+	palSetPadMode(GPIOA, 7, PAL_MODE_OUTPUT_PUSHPULL);
 
-		get_mag(mag);
+	while (TRUE) {
+		time += MS2ST(1000);
+		counter++;
 
 		sprintf(txbuf, "X: %d  Y: %d  Z: %d\r\n", (int) mag[0], (int) mag[1], (int) mag[2]);
 		uartStartSend(&UARTD1, sizeof(txbuf), txbuf);
@@ -44,6 +45,13 @@ static msg_t comm_thread(void *arg)
 		palSetPad(GPIOD, GPIOD_LED4);
 		chThdSleepMilliseconds(50);
 		palClearPad(GPIOD, GPIOD_LED4);
+
+		palSetPad(GPIOA, 7);
+		chThdSleepMilliseconds(50);
+		palClearPad(GPIOA, 7);
+		palSetPad(GPIOA, 6);
+		chThdSleepMilliseconds(50);
+		palClearPad(GPIOA, 6);
 
 		chThdSleepUntil(time);
 	}
@@ -54,7 +62,7 @@ static msg_t comm_thread(void *arg)
 /*
  * Second communications loop
  */
-static WORKING_AREA(wa_comm_thread_2, 128);
+static WORKING_AREA(wa_comm_thread_2, 512);
 static msg_t comm_thread_2(void *arg)
 {
 	(void) arg;
@@ -62,24 +70,14 @@ static msg_t comm_thread_2(void *arg)
 	systime_t time = chTimeNow();
 	int counter = 0;
 
-	char txbuf[20];
-
-	palSetPadMode(GPIOA, 6, PAL_MODE_OUTPUT_PUSHPULL);
-	palSetPadMode(GPIOA, 7, PAL_MODE_OUTPUT_PUSHPULL);
+	char txbuf[200];
 
 	while (TRUE) {
-		time += MS2ST(234);
+		time += MS2ST(100);
 		counter++;
 
-		sprintf(txbuf, "Je vis aussi!\r\n");
+		debug_ahrs(txbuf);
 		uartStartSend(&UARTD3, sizeof(txbuf), txbuf);
-
-		palSetPad(GPIOA, 7);
-		chThdSleepMilliseconds(50);
-		palClearPad(GPIOA, 7);
-		palSetPad(GPIOA, 6);
-		chThdSleepMilliseconds(50);
-		palClearPad(GPIOA, 6);
 
 		chThdSleepUntil(time);
 	}
@@ -117,35 +115,31 @@ static msg_t adc_thread(void *arg)
 /*
  * Control loop
  */
+#define CONTROL_DT 0.005
 static WORKING_AREA(wa_control_thread, 128);
 static msg_t control_thread(void *arg)
 {
 	(void) arg;
 	chRegSetThreadName("control");
 
-	setup_ahrs();
-
-	setup_motors();
-
 	systime_t time = chTimeNow();
 	float i[4];
 	i[0] = 0;
-	i[1] = 250;
-	i[2] = 500;
-	i[3] = 750;
+	i[1] = 1/CONTROL_DT;
+	i[2] = 2/CONTROL_DT;
+	i[3] = 3/CONTROL_DT;
 	uint8_t j = 0;
-	float dir[4];
-	dir[0] = 0.2;
-	dir[1] = 0.2;
-	dir[2] = 0.2;
-	dir[3] = 0.2;
+	float step[4];
+	step[0] = CONTROL_DT*1000;
+	step[1] = CONTROL_DT*1000;
+	step[2] = CONTROL_DT*1000;
+	step[3] = CONTROL_DT*1000;
 
 	while (TRUE) {
-		time += MS2ST(1);   // Next deadline in 1 ms.
+		time += MS2ST(CONTROL_DT*1000);   // Next deadline in 1 ms.
 		for (j=0; j<4; j++) {
-			i[j] += dir[j];
-			if (i[j] > 1000.0) dir[j] = -0.2;
-			if (i[j] < 0.0) dir[j] = 0.2;
+			i[j] += step[j];
+			if (i[j] > 600.0 || i[j] < 0.0) step[j] = -step[j];
 		}
 
 		update_ahrs();
@@ -177,6 +171,10 @@ int main(void)
 	setup_uart();
 
 	setup_adc();
+
+	setup_spi();
+
+	setup_ahrs();
 
 	setup_motors();
 
