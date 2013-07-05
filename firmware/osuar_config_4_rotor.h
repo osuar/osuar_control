b96c77690587b96776980f94518e64c4ebc38920
Soo-Hyun Yoo <yoos117@gmail.com>
2013-07-05 05:18:20 -0700

    Set preliminary PID gains.

--- firmware/osuar_config_4_rotor.h
+++ firmware/osuar_config_4_rotor.h
@@ -3,6 +3,14 @@
 
 #define THROTTLE_CAP 0.7   // Maximum duty cycle allowed for any of the rotors.
 
+/* Controller gains */
+#define ANG_POS_KP 1.0
+#define ANG_POS_KI 0.0
+#define ANG_POS_KD 0.0
+#define ANG_VEL_KP 0.2
+#define ANG_VEL_KI 0.0
+#define ANG_VEL_KD 0.0
+
 // Rotor indices by angle from positive X. For example, I_R045 refers to the
 // rotor at 45 degrees, i.e., front left.
 #define I_R045 0
