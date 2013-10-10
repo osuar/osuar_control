bb7eb99f08119c8eba7c88be06744e20c4852d2b
Soo-Hyun Yoo <yoos117@gmail.com>
2013-10-09 23:54:33 -0700

    Tune Hermes' control gains.

--- firmware/osuar_config_hermes.h
+++ firmware/osuar_config_hermes.h
@@ -4,10 +4,10 @@
 #define THROTTLE_CAP 1.0
 
 /* Controller gains */
-#define ANG_POS_KP 1.0
+#define ANG_POS_KP 0.4
 #define ANG_POS_KI 0.0
 #define ANG_POS_KD 0.0
-#define ANG_VEL_KP 0.2
+#define ANG_VEL_KP 0.05
 #define ANG_VEL_KI 0.0
 #define ANG_VEL_KD 0.0
 
