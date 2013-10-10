ef9ec2faed127747fea2c089a11cc9090a3384c0
Soo-Hyun Yoo <yoos117@gmail.com>
2013-10-10 00:06:49 -0700

    Update config for Zephyrus.

--- firmware/osuar_config_zephyrus.h
+++ firmware/osuar_config_zephyrus.h
@@ -1,10 +1,34 @@
 #define NUM_ROTORS 3
 #define ESC_COMM PWM
 
-#define GYR_X_OFFSET  0.040300
-#define GYR_Y_OFFSET  0.021200
-#define GYR_Z_OFFSET  0.115000
+#define THROTTLE_CAP 1.0
+
+/* Controller gains */
+#define ANG_POS_KP 1.0
+#define ANG_POS_KI 0.0
+#define ANG_POS_KD 0.0
+#define ANG_VEL_KP 0.2
+#define ANG_VEL_KI 0.0
+#define ANG_VEL_KD 0.0
+
+/* IMU offsets */
+#define GYR_X_OFFSET  0.043080
+#define GYR_Y_OFFSET  0.014490
+#define GYR_Z_OFFSET  0.010010
 #define ACC_X_OFFSET  0.018000
 #define ACC_Y_OFFSET  0.000000
 #define ACC_Z_OFFSET  0.115000
+#define GYR_X ( raw_gyr[0])
+#define GYR_Y ( raw_gyr[1])
+#define GYR_Z ( raw_gyr[2])
+#define ACC_X ( raw_acc[0])
+#define ACC_Y ( raw_acc[1])
+#define ACC_Z ( raw_acc[2])
+
+/* PWM duty cycle constants */
+#define MOTOR_PWM_DISABLED 0.50
+#define MOTOR_PWM_MIN 0.55
+#define MOTOR_PWM_MAX 0.99
+#define SERVO_PWM_MIN 0.0517
+#define SERVO_PWM_MAX 0.0944
 
