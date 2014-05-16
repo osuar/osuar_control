18e7b1120319fff1c209a5e6a6f03b6b986ed8cd
Soo-Hyun Yoo <yoos117@gmail.com>
2014-05-16 05:12:08 -0700

    Handle received packets.

--- firmware/osuar_comm.c
+++ firmware/osuar_comm.c
@@ -5,18 +5,10 @@
  */
 
 #include <osuar_comm.h>
+#include <osuar_protocol.h>
 
 #define CONTROL_PACKET_SIZE 10
 #define PACKET_BUFFER_SIZE (CONTROL_PACKET_SIZE * 2)
-
-uint8_t remote_comm_rxbuf[4];
-uint8_t remote_comm_txbuf[4];
-
-/* Circular buffer that continually takes data from the incoming rxbuf. */
-static uint8_t packetbuf[PACKET_BUFFER_SIZE];
-
-/* Current location of the write head in the buffer. */
-static size_t write_idx;
 
 void setup_comm(void)
 {
@@ -34,63 +26,51 @@ void setup_comm(void)
 	palSetPadMode(GPIOB, 11, PAL_MODE_ALTERNATE(7));   // USART3 RX
 }
 
-void osuar_comm_data_received(BaseSequentialStream *sd)
+void osuar_comm_handle_receive(uint8_t *rxbuf, uint8_t num, osuar_rb_t *recv_rb)
 {
-	(void) sd;
+	static osuar_packet_t packet_up;   /* Received uplink packet */
+	static uint8_t msg_type;   /* Received message type */
+	static void *msg;
 
-	size_t i;
-        for(i = 0; i < sizeof(remote_comm_rxbuf); i++) {
-		packetbuf[write_idx] = remote_comm_rxbuf[i];
-		write_idx = (write_idx + 1) % PACKET_BUFFER_SIZE;
-        }
-
-	// TODO(cesarek): warn if write_idx overlaps read_idx?
-
-        // UART driver goes into idle mode when the buffer is filled.
-	//uartStartReceive(&UARTD3, sizeof(remote_comm_rxbuf), remote_comm_rxbuf);
-}
-
-bool osuar_comm_parse_input(float *throttle, float ang_pos[3])
-{
-	// TODO(cesarek): Figure out a header/trailer that can't happen in
-	// normal operation
-
-	// Find the first starting index of the first packet
-	size_t start_idx;
-	for(start_idx = 0; start_idx < sizeof(packetbuf); start_idx++) {
-		// Start magic sequence: 0xFF, 0xFF, 0xFF
-		if(packetbuf[(start_idx + 0) % PACKET_BUFFER_SIZE] == 0xFF
-				&& packetbuf[(start_idx + 1) % PACKET_BUFFER_SIZE] == 0xFF
-				&& packetbuf[(start_idx + 2) % PACKET_BUFFER_SIZE] == 0xFF) {
+	osuar_rb_add(recv_rb, rxbuf, num);   /* TODO(yoos): handle failure */
+	if (protocol_get_packet(recv_rb, &packet_up, &msg_type)) {
+		msg = &packet_up.message;
+		switch(msg_type) {
+		case(UP_COMMAND_TYPE):
+			osuar_comm_parse_command(
+					((up_command_t*) msg)->mode,
+					((up_command_t*) msg)->throttle,
+					((up_command_t*) msg)->axes
+					);
+			break;
+		case(UP_CONFIG_TYPE):
+			/* TODO(yoos) */
+			break;
+		default:
 			break;
 		}
 	}
+}
 
-	// Check if no header was found
-	if(start_idx == sizeof(packetbuf)) {
-		return false;
-	}
-
-	// Check the packet trailer sequence
-	if(packetbuf[(start_idx + 7) % PACKET_BUFFER_SIZE] != 0x00
-			|| packetbuf[(start_idx + 8) % PACKET_BUFFER_SIZE] != 0x00
-			|| packetbuf[(start_idx + 9) % PACKET_BUFFER_SIZE] != 0x00) {
-		return false;
-	}
-
+bool osuar_comm_parse_command(uint8_t mode, uint8_t throttle, uint8_t ang_pos[3])
+{
+	(void) mode;
+	(void) throttle;
+	(void) ang_pos;
+	/* TODO: math */
 	// Parse the command
 	// TODO(cesarek): Restrict to 1 <= x <= 127 - 1 to prevent
 	// header/trailer ambiguity?
-	ang_pos[0] = ((int8_t) packetbuf[(start_idx + 3) % PACKET_BUFFER_SIZE]) / 127.0;
-	ang_pos[1] = ((int8_t) packetbuf[(start_idx + 4) % PACKET_BUFFER_SIZE]) / 127.0;
-	ang_pos[2] = ((int8_t) packetbuf[(start_idx + 5) % PACKET_BUFFER_SIZE]) / 127.0;
-	*throttle = ((int8_t) packetbuf[(start_idx + 6) % PACKET_BUFFER_SIZE]) / 127.0;
+	//ang_pos[0] = ((int8_t) packetbuf[(start_idx + 3) % PACKET_BUFFER_SIZE]) / 127.0;
+	//ang_pos[1] = ((int8_t) packetbuf[(start_idx + 4) % PACKET_BUFFER_SIZE]) / 127.0;
+	//ang_pos[2] = ((int8_t) packetbuf[(start_idx + 5) % PACKET_BUFFER_SIZE]) / 127.0;
+	//*throttle = ((int8_t) packetbuf[(start_idx + 6) % PACKET_BUFFER_SIZE]) / 127.0;
 
-        size_t i;
-        for(i = 0; i < CONTROL_PACKET_SIZE; i++) {
-          // TODO: drops packets when setting this to 0x00, why?
-          packetbuf[(start_idx + i) % PACKET_BUFFER_SIZE] = 0x01;
-        }
+    //    size_t i;
+    //    for(i = 0; i < CONTROL_PACKET_SIZE; i++) {
+    //      // TODO: drops packets when setting this to 0x00, why?
+    //      packetbuf[(start_idx + i) % PACKET_BUFFER_SIZE] = 0x01;
+    //    }
 
 	// TODO(cesarek): clear this portion of the buffer?
 
--- firmware/osuar_comm.h
+++ firmware/osuar_comm.h
@@ -7,6 +7,7 @@
 #ifndef OSUAR_COMM_H
 #define OSUAR_COMM_H
 
+#include <osuar_ringbuffer.h>
 #include <hal.h>
 
 /*
@@ -14,8 +15,15 @@
  */
 void setup_comm(void);
 
-void osuar_comm_data_received(BaseSequentialStream *sd);
-bool osuar_comm_parse_input(float *throttle, float control_values[3]);
+/*
+ * @brief Put received bytes in a ringbuffer for processing.
+ *
+ * @param rxbuf Small buffer storing latest few bytes off serial RX buffer.
+ * @param num Number of bytes received.
+ * @param recv_rb Ringbuffer in which to store received bytes.
+ */
+void osuar_comm_handle_receive(uint8_t *rxbuf, uint8_t num, osuar_rb_t *recv_rb);
+bool osuar_comm_parse_command(uint8_t mode, uint8_t throttle, uint8_t ang_pos[3]);
 
 /*
  * Utility function to clear (i.e., zero) a buffer.
